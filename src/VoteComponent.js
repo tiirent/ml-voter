68aba90db44765959eaf7a205260141acc8e0d3b
Trent <nguy2284@umn.edu>
2020-09-29 12:35:04 -0500

    add kimmy buttons

--- src/VoteComponent.js
+++ src/VoteComponent.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import redkimmy from './images/redKimmy.png'
+import bluekimmy from './images/blueKimmy.png'
 
 class VoteComponent extends React.Component {
   constructor(props) {
@@ -7,25 +9,38 @@ class VoteComponent extends React.Component {
     this.state = {value: ''};
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleRedKimmy = this.handleRedKimmy.bind(this);
+    this.handleBlueKimmy = this.handleBlueKimmy.bind(this);
   }
 
   handleChange(event) {
     this.setState({value: event.target.value});
   }
 
-  handleSubmit(event) {
+  handleRedKimmy(event) {
     let baseUrl = "https://gtop100.com/topsites/MapleStory/sitedetails/MapleLegends-The-Old-School-MapleStory-Server-MAC-Compatible-16-9-Client-Summer-Events-87398?vote=1&pingUsername=" + this.state.value;
-    window.open(baseUrl, "_blank")
+    window.open(baseUrl, "_blank");
+    event.preventDefault();
+  }
+
+  handleBlueKimmy(event) {
+    let baseUrl = "https://topg.org/maplestory-private-servers/in-605064-" + this.state.value;
+    window.open(baseUrl, "_blank");
     event.preventDefault();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>Account:</label>
-        <input type="text" autocomplete="on" value={this.state.value} onChange={this.handleChange} />
-        <input type="submit" value="Submit" />
+      <form onSubmit={this.handleRedKimmy}>
+        <table>
+          <tr>
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </tr>
+          <tr>
+            <img src = {redkimmy} onClick={this.handleRedKimmy}/>
+            <img src = {bluekimmy} onClick={this.handleBlueKimmy}/>
+          </tr>
+        </table>
       </form>
     );
   }
